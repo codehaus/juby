fa9c93ce5e33e90cb3f1a00382a3a60195db2eb8
garrett <garrett@33d7e446-a00d-0410-afe1-803f2e590d38>
2006-03-09 16:24:09 +0000

    Simple fix to setup.sh so that it runs

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 VERSION=1.5
 
-#ruby ./extconf.rb --with-jdk-include=/System/Library/Frameworks/JavaVM.framework/Versions/$VERSION/Headers/ --with-jdk-lib=/System/Library/Frameworks/JavaVM.framework/Versions/$VERSION/Libraries    
-ruby ./extconf.rb --with-jdk-include=/System/Library/Frameworks/JavaVM.framework/Headers/ --with-jdk-lib=/System/Library/Frameworks/JavaVM.framework/Libraries    
+#ruby ./ext/extconf.rb --with-jdk-include=/System/Library/Frameworks/JavaVM.framework/Versions/$VERSION/Headers/ --with-jdk-lib=/System/Library/Frameworks/JavaVM.framework/Versions/$VERSION/Libraries    
+ruby ./ext/extconf.rb --with-jdk-include=/System/Library/Frameworks/JavaVM.framework/Headers/ --with-jdk-lib=/System/Library/Frameworks/JavaVM.framework/Libraries    
