cf8dec11a9fce7f577226f054fba2ed94ecb81b8
bob <bob@33d7e446-a00d-0410-afe1-803f2e590d38>
2006-03-11 23:20:24 +0000

    Friendlier output from test to insure sanity

--- tests/tc_misc.rb
+++ tests/tc_misc.rb
@@ -4,11 +4,16 @@ require 'test/unit'
 require 'juby'
 
 class TestStatic < Test::Unit::TestCase
-  def test_simple
+  def test_java_version_property
     Java::initialize_vm
     props = Java::java.lang.System.properties   
     assert_not_nil( props[ 'java.version' ] )
     assert_nil( props['something.cheesy.this.way.comes'] )
-    puts "java.version #{props['java.version']}"
+    puts "  ------------------------------------"
+    puts "  Your installation of Juby has been"
+    puts "  deemed to be sane." 
+    puts ""
+    puts "  java.version #{props['java.version']}"
+    puts "  ------------------------------------"
   end
 end
